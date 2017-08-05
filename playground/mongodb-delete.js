3d3570247764d971eae8333579882b7237de23e9
Maaz <maazupial@gmail.com>
2017-08-05 09:14:38 +0530

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
+if (err)	 {
+	return console.log('Unable to connect');
+}
+
+console.log('Connected to mongodb server')
+
+// deleteMany
+
+// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
+// 	console.log(result);
+// })
+
+//deleteOne
+// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+// 	console.log(result);
+// })
+
+// findOneAndDelete
+// db.collection('Todos').findOneAndDelete({completed: false}).then((result)=>{
+// 	console.log(result)
+// });
+
+// db.collection('Users').deleteMany({name: 'Maaz'}).then((results)=>{
+// 	console.log(results);
+// })
+
+db.collection('Users').findOneAndDelete({
+	_id: new ObjectID("5985473adc9e37169cfbe719")
+}).then((result)=>{
+	console.log(result);
+});
+
+// db.close();
+});
