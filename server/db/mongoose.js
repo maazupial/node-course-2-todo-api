a0e23ad5911621db6543029e3e482f2c20e5dd68
Maaz <maazupial@gmail.com>
2017-08-13 08:36:24 +0530

    Setup separate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://maazupial:Supersonic1@ds123933.mlab.com:23933/star-wars-quotes' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://maazupial:Supersonic1@ds123933.mlab.com:23933/star-wars-quotes');
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports.mongoose = {mongoose};
